2150bca5e1711b8201b29cae9686bcdf85ef5e7c
douya <douya@stu.pku.edu.cn>
2022-10-14 12:57:15 +0000

    update ignore and py

--- test_old.py
+++ test_old.py
@@ -1,12 +1,12 @@
 import os
-os.system("autotest -koopa -s lv1 /root/compiler")
-variable = input("continue")
-os.system("autotest -riscv -s lv1 /root/compiler")
-variable = input("continue")
-os.system("autotest -koopa -s lv3 /root/compiler")
-variable = input("continue")
-os.system("autotest -riscv -s lv3 /root/compiler")
-variable = input("continue")
-os.system("autotest -koopa -s lv4 /root/compiler")
-variable = input("continue")
-os.system("autotest -riscv -s lv4 /root/compiler")
+count = 0
+cmd_lines = ["autotest -koopa -s lv1 /root/compiler",
+             "autotest -riscv -s lv1 /root/compiler",
+             "autotest -koopa -s lv3 /root/compiler",
+             "autotest -riscv -s lv3 /root/compiler",
+             "autotest -koopa -s lv4 /root/compiler",
+             "autotest -riscv -s lv4 /root/compiler"]
+for cmd_line in cmd_lines:
+    count = count+1
+    os.system(cmd_line)
+    variable = input(str(count)+"/"+str(len(cmd_lines)))
